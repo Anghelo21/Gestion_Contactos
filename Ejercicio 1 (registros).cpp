19ebcbbb484982609357a17cd14096520c0cdacb
AngheloCM <angheloprog@gmail.com>
2025-06-28 01:49:28 -0500

    Añadiendo menú de opciones y definiendo el nombre las funciones

--- Ejercicio 1 (registros).cpp
+++ Ejercicio 1 (registros).cpp
@@ -8,5 +8,37 @@ struct contactoEmail{
 };
 
 int main(){
+	int opcion;
+	do{
+		cout<<" --- Menu --- "<<endl;
+		cout<<"1. Agregar contacto"<<endl;
+		cout<<"2. Eliminar contacto"<<endl;
+		cout<<"3. Mostrar lista general de contactos"<<endl;
+		cout<<"4. Mostrar contactos ordenados por correo"<<endl;
+		cout<<"5. Salir del programa"<<endl;
+		cout<<"Seleccione una opcion"<<endl;
+		cin>>opcion;
+		cin.ignore();
+		switch(opcion){
+			case 1:
+				//agregar();
+				break;
+			case 2:
+				//eliminar();
+				break;
+			case 3:
+				//mostrar();
+				break;
+			case 4:
+				//orden();
+				break;
+			case 5:
+				cout<<"Saliendo..."<<endl;
+				break;
+			default:
+				cout<<"Opcion invalida"<<endl;
+				break;			
+		}
+	} while(opcion!=5);
 	return 0;
 }
