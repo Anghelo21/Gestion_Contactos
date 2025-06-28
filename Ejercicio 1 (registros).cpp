5452e8e163c1cdae692cdee15704ab10951d7852
AngheloCM <angheloprog@gmail.com>
2025-06-28 01:19:19 -0500

    Creando la estructura contactoEmail y definiendo variables

---
+++ Ejercicio 1 (registros).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct contactoEmail{
+	string nombre, sexo, email, nacionalidad;
+	int edad, telefono;
+};
+
+int main(){
+	return 0;
+}
